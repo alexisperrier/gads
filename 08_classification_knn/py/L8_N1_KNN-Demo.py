08223a5b78146b9bdf1e95a554c199349c50a75f
Alexis Perrier <aperrier@berklee.edu>
2016-07-09 11:31:00 -0400

    L6 + L8

---
+++ 08_classification_knn/py/L8_N1_KNN-Demo.py
@@ -0,0 +1,34 @@
+# Notebook L8 N1 Iris dataset Demo: KNN In Action
+
+from sklearn.metrics import accuracy_score
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# shuffle
+np.random.seed(8)
+# [1 to 150]
+idx = np.arange(X.shape[0])
+np.random.shuffle(idx)
+X = X[idx]
+y = y[idx]
+
+# Train test
+X_train = X[:100]; y_train = y[:100]; X_test = X[100:]; y_test = y[100:]
+
+# K = 5
+clf = neighbors.KNeighborsClassifier(5)
+clf.fit(X_train, y_train)
+
+y_hat= clf.predict(X_test)
+
+#
+acc = []
+for k in range(2,20, 1):
+    clf = neighbors.KNeighborsClassifier(k)
+    clf.fit(X_train, y_train)
+    y_hat = clf.predict(X_test)
+    score = accuracy_score(y_test, y_hat )
+    acc.append(score)
+    print("k: %s accuracy: %0.2f missed %s"% (k, score, np.count_nonzero(y_hat - y_test))   )
