77a4b8bfff1abe892472ce5de54c80f1dfc1556e
Alexis Perrier <aperrier@berklee.edu>
2016-07-16 18:49:42 -0400

    L9; L10 Clustering

--- 09_logistic_regression/py/L9-logistic-regression.py
+++ 09_logistic_regression/py/L9-logistic-regression.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.grid_search import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics  import confusion_matrix
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
@@ -103,4 +103,126 @@ print("Default Probability for Student: %0.4f"%  p )
 yy = clf.params['const'] + clf.params['balance'] * 1500 +  clf.params['income'] * 40
 p = np.exp(yy) / (1+ np.exp(yy))
 print("Default Probability for Non Student: %0.4f"%  p )
+
+# --------------------------------------------------------------------------------
+#  Confusion Matrix in Scikit
+# --------------------------------------------------------------------------------
+
+from sklearn.metrics import confusion_matrix
+y_true = [0,0,0,0,0,1,1,1,1,1]
+y_pred = [0,0,0,1,1,0,1,1,1,1]
+confusion_matrix(y_true, y_pred)
+
+# ----------------------------------------------------------------------------
+#  Logistic Regression
+# ----------------------------------------------------------------------------
+from sklearn.cross_validation import train_test_split
+
+df = pd.read_csv('../data/Default.csv', index_col = False)
+df['default_fact']   = df.default.factorize()[0]
+df['student_fact']   = df.student.factorize()[0]
+
+X = df[['balance', 'income', 'student_fact']]
+y = df.default_fact
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=88)
+
+lr = LogisticRegression()
+parameters = {'solver': ['newton-cg','lbfgs','liblinear','sag']}
+
+clf = GridSearchCV(lr, parameters, cv = 5)
+
+clf.fit(X_train, y_train)
+
+best_clf = clf.best_estimator_
+
+y_hat = best_clf.predict(X_test)
+print(confusion_matrix(y_test, y_hat) )
+
+print("Best accuracy score for Logistic Regression: %0.4f"% accuracy_score(y_test, y_hat) )
+
+# ----------------------------------------------------------------------------
+#  K-NN
+# ----------------------------------------------------------------------------
+
+knn = KNeighborsClassifier()
+parameters = {'n_neighbors': [2,5,10] }
+
+clf = GridSearchCV(knn, parameters, cv = 5)
+
+clf.fit(X_train, y_train)
+
+best_knn = clf.best_estimator_
+
+y_hat = best_knn.predict(X_test)
+
+print(confusion_matrix(y_test, y_hat) )
+
+print("Best accuracy score for K-NN (K:%s): %0.4f"%  (str(clf.best_params_),  accuracy_score(y_test, y_hat) ) )
+
+# ----------------------------------------------------------------------------
+#  Predict, Predict_proba and the question of the threshold
+# ----------------------------------------------------------------------------
+
+pred = pd.DataFrame( {'y_hat':y_hat, 'y_proba': y_proba[:,1] }  )
+
+fig = plt.figure(figsize=(12,9))
+ax1 = plt.subplot(121)
+ax2 = plt.subplot(122)
+c_palette = {0:'lightblue', 1:'orange'}
+sns.boxplot('y_hat', 'y_proba', data=pred[y_hat == 0], orient='v', ax=ax1, palette=c_palette)
+sns.boxplot('y_hat', 'y_proba', data=pred[y_hat == 1], orient='v', ax=ax2, palette=c_palette)
+
+# Other threshold
+t = 0.4
+
+def threshold(x):
+    if x < t:
+        return 0
+    else:
+        return 1
+
+pred['y_hat_t'] = pred.y_proba.apply(lambda x : threshold(x)  )
+
+print(confusion_matrix(y_test, pred.y_hat_t))
+print(confusion_matrix(y_test, pred.y_hat))
+
+print(accuracy_score(y_test, pred.y_hat_t))
+print(accuracy_score(y_test, pred.y_hat))
+
+# Look at many thresholds
+
+for t in np.linspace(0.1, 1, 10):
+    pred['y_hat_t'] = pred.y_proba.apply(lambda x : threshold(x)  )
+    print(" Threshold %s, accuracy, %0.4f "% (t, accuracy_score(y_test, pred.y_hat_t)) )
+
+
+# ----------------------------------------------------------------------------
+#  ROC Curve http://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_curve.html
+# ----------------------------------------------------------------------------
+
+from sklearn.metrics import roc_curve, auc
+
+
+fpr, tpr, thresholds = roc_curve(y_test, pred.y_proba)
+
+roc_auc = auc(fpr, tpr)
+
+
+plt.figure()
+plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
+
+# with best_knn
+
+y_proba = best_knn.predict_proba(X_test)[:,1]
+
+
 
---
+++ 10_unsupervised/py/L10_N1_unsupervised-k-means.py
@@ -0,0 +1,67 @@
+# ----------------------------------------------------------------------------
+#  Lesson 10.1: Unsupervised - K Means Clustering
+# ----------------------------------------------------------------------------
+import numpy as np
+import pandas as pd
+
+from sklearn import cluster, datasets
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.metrics import accuracy_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+
+import matplotlib.pyplot as plt
+%matplotlib
+import seaborn as sns
+plt.style.use('fivethirtyeight')
+
+
+np.random.seed(8)
+
+# ----------------------------------------------------------------------------
+#  The four datasets, colors and algorithms
+# ----------------------------------------------------------------------------
+
+n_samples = 1500
+noisy_circles   = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
+noisy_moons     = datasets.make_moons(n_samples=n_samples, noise=.05)
+blobs           = datasets.make_blobs(n_samples=n_samples, random_state=8)
+no_structure    = np.random.rand(n_samples, 2), None
+
+
+# Initialize colors
+colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
+colors = np.hstack([colors] * 20)
+
+# The algorithms
+kmeans = cluster.KMeans(n_clusters=2)
+dbscan = cluster.DBSCAN(eps=.2)
+spectral = cluster.SpectralClustering(n_clusters=2, eigen_solver='arpack', affinity="nearest_neighbors")
+
+
+# clst = kmeans, dbscan, spectral
+
+def cluster(clst, title):
+    # Create  2x2 figure for each algorithm
+    fig, ax = plt.subplots(2,2, figsize=(12,12)  )
+    datasets = [noisy_circles, noisy_moons, blobs, no_structure]
+    plot_num = 1
+    for i, dataset in enumerate(datasets):
+
+        X, y = dataset
+        X = StandardScaler().fit_transform(X)
+        clst.fit(X)
+        y_pred = clst.labels_.astype(np.int)
+        plt.subplot(2, 2, plot_num)
+
+        plt.scatter(X[:, 0], X[:, 1], color=colors[y_pred].tolist(), s=10)
+        plt.title(title)
+        plot_num +=1
+
+cluster(kmeans, 'Kmeans')
+cluster(dbscan, 'DBscan')
+cluster(spectral,'Spectral')
+
+
