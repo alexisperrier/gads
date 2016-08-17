3323e301a02b581f97836cf657fad5a34b5c81eb
Alexis Perrier <aperrier@berklee.edu>
2016-08-17 19:01:14 -0400

    L18 R

---
+++ 18_R_AWSML/R/timeseries_exploration.R
@@ -0,0 +1,72 @@
+
+# Load the data
+tui <- read.csv('../data/FSE-TUI1_X.csv')
+
+colnames(tui)
+# plot Closing Price
+plot(tui$Close, type='l', main='TUI Close', lwd=1, col='red', ylab='closing')
+
+# plot log
+plot(log(tui$Close), type='l', main='TUI Close', lwd=1, col='red', ylab='closing')
+
+# plot diff of the Log of the closing price
+plot(diff(tui$Close), type='l', main='TUI Close', lwd=1, col='red', ylab='closing')
+
+hist( diff(log(tui$Close)), breaks = 50, prob = T)
+lines(density(diff(log(tui$Close))))
+
+# --------------------------------------------------------
+# Compare density with Normal distribution
+# --------------------------------------------------------
+data <- diff(log(tui$Close))
+hist( data, breaks = 50, prob = T)
+lines(density(data), col='red', lwd = 2)
+
+# mean and std
+mu <- mean(data)
+sigma <- sd(data)
+
+# normal distribution
+x <- seq(-3,3, length=100)
+y <- dnorm(x,mu,sigma)
+
+lines(x,y,lwd=2, col='blue')
+
+# QQ plot
+qqnorm(data)
+abline(0,1)
+
+# Kolmogorov Smirnov test for normality (H0: normal distribution)
+ks.test(data, 'pnorm', mu, sigma)
+# Shapiro Wilk test for normality
+shapiro.test(data)
+
+# ------------------------------------------------------
+# Moving Average : filter(data,filter = rep(1/n,n))
+# ------------------------------------------------------
+data <- tui$Close[1000:1300]
+tui.5 <- filter(data,filter = rep(1/5,5))
+tui.25 <- filter(data,filter = rep(1/25,25))
+tui.81 <- filter(data,filter = rep(1/81,81))
+plot(data, type='n')
+points(data, pch=16, cex=0.5)
+lines(tui.5, col='red', lwd=2)
+lines(tui.25, col='blue', lwd=2)
+lines(tui.81, col='green', lwd=2)
+
+# -------------------------------------------------------
+# Decomposition with stl
+# Beer production in Australia
+# https://datamarket.com/data/set/22xr/monthly-beer-production-in-australia-megalitres-includes-ale-and-stout-does-not-include-beverages-with-alcohol-percentage-less-than-115-jan-1956-aug-1995#!ds=22xr&display=line
+# -------------------------------------------------------
+
+df <- read.csv('../data/monthly-beer-production-in-austr.csv')
+beer <- ts(df$production, freq=12)
+res <- stl(beer, s.window='periodic')
+plot(stl(beer, s.window='periodic'))
+
+# Find periodicity, why 12?
+install.packages('xts')
+library(xts)
+periodicity(ts(df$production))
+
